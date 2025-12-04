38e6cbb7b429883fd73493543403c28c7a180bd2
Evan <heckler82@gmail.com>
2025-12-03 21:06:37 -0900

    Solution for day 4 puzzles

---
+++ src/main/java/com/baker/billy/years/year2025/Puzzle4.java
@@ -0,0 +1,73 @@
+package com.baker.billy.years.year2025;
+
+import com.baker.billy.core.Puzzle;
+
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class Puzzle4 extends Puzzle {
+    private final Map<Point, Set<Point>> graph = new HashMap<>();
+
+    /**
+     * Creates a new puzzle
+     *
+     * @param year The year
+     */
+    public Puzzle4(int year) {
+        super(year);
+    }
+
+    @Override
+    protected void task1() {
+        int y = 0;
+        for(String str : input) {
+            int x = 0;
+            for(char c : str.toCharArray()) {
+                if(c == '@') {
+                    Point p = new Point(x, y);
+                    graph.put(p, new HashSet<>());
+                    for (var key : graph.keySet()) {
+                        double distSq = Math.pow((key.x - x), 2) + Math.pow((key.y - y), 2);
+                        if (distSq <= 2 && distSq != 0) {
+                            graph.get(key).add(p);
+                            graph.get(p).add(key);
+                        }
+                    }
+                }
+                x++;
+            }
+            y++;
+        }
+
+        long total = graph.keySet().stream()
+                .filter(k -> graph.get(k).size() < 4)
+                .toList()
+                .size();
+
+        System.out.printf("There are %d rolls that can be moved\n", total);
+    }
+
+    @Override
+    protected void task2() {
+        int total = 0;
+        List<Point> toRemove;
+        do {
+            toRemove = graph.keySet().stream()
+                    .filter(k -> graph.get(k).size() < 4)
+                    .toList();
+            Consumer<Point> removeElement = k -> {
+                graph.remove(k);
+                graph.keySet().forEach(k2 -> graph.get(k2).remove(k));
+            };
+            toRemove.forEach(removeElement);
+            total += toRemove.size();
+        } while(!toRemove.isEmpty());
+
+        System.out.printf("There are %d rolls that can be removed\n", total);
+    }
+}
